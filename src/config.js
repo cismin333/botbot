524692b0c74f010d9e6a904e88d15a4fe608be9d
cismin333 <32226141+cismin333@users.noreply.github.com>
2018-04-03 18:58:50 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  channelAccessToken: "",
-  channelSecret: ""
+  channelAccessToken: "htzpJLyLiNExpCCuusM6p79w/GTr2bRMI1B/T+2J4oegYBwu6A/M4TuBgSAymhDfxUUOGZyKg2WYzQKFAZW5movSNRq1hdXMBOxi26SSUlpC6amZCKqAoVWNVXNrssIZC0IsvZ9Gd43Qe0m1hh045QdB04t89/1O/w1cDnyilFU=",
+  channelSecret: "372542c0bc529dbaa59b8d436a62d72e"
 };
 
 export default config;
